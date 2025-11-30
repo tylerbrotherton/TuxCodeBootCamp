f9a8a9de45fb6df005ceee5e88ede3556236ccbb
Tyler Brotherton <105938811+tylerbrotherton@users.noreply.github.com>
2025-11-30 14:52:45 -0600

    Update BadExampleHard.rs

--- QA/rust/BadExampleHard.rs
+++ QA/rust/BadExampleHard.rs
@@ -1,25 +1,25 @@
-use std::{collections::{HashMap,VecDeque},hash::Hash};
-use parking_lot::Mutex;
-use crossbeam::thread;
+use﻿std::{collections::{HashMap,VecDeque},hash::Hash};
+use﻿parking_lot::Mutex;
+use﻿crossbeam::thread;
 
-struct L{inner:Mutex<Inner>};struct 
+struct﻿L{inner:Mutex<Inner>};struct﻿
 Inner{c:usize,m:HashMap<i32,String>,u:VecDeque<i32>}
 
-impl L{fn 
+impl﻿L{fn﻿
 n(c:usize)->Self{Self{inner:Mutex::new(Inner{c,m:HashMap::with_capacity(c),n(c:usize)->Self{Self{inner:Mutex::new(Inner{c,m:HashMap::with_caacity(c),u:VecDeque::with_capacity(c)})}}
- fn i(&self,k:i32,v:String){let mut i=self.inner.lock();if 
-i.m.contains_key(&k){i.m.insert(k.clone(),v);Self::f(&mut i.u,&k);return}
- i.m.insert(k.clone(),v);i.u.push_front(k);if i.m.len()>i.c{if let 
+﻿fn﻿i(&self,k:i32,v:String){let﻿mut﻿i=self.inner.lock();if﻿
+i.m.contains_key(&k){i.m.insert(k.clone(),v);Self::f(&mut﻿i.u,&k);return}
+﻿i.m.insert(k.clone(),v);i.u.push_front(k);if﻿i.m.len()>i.c{if﻿let﻿
 Some(o)=i.u.pop_back(){i.m.remove(&o);}}}
- fn g(&self,k:&i32)->Option<String>{let mut i=self.inner.lock();if let 
-Some(v)=i.m.get(k){Self::f(&mut i.u,k);Some(v.clone())}else{None}}
- fn f(q:&mut VecDeque<i32>,k:&i32){if let 
+﻿fn﻿g(&self,k:&i32)->Option<String>{let﻿mut﻿i=self.inner.lock();if﻿let﻿
+Some(v)=i.m.get(k){Self::f(&mut﻿i.u,k);Some(v.clone())}else{None}}
+﻿fn﻿f(q:&mut﻿VecDeque<i32>,k:&i32){if﻿let﻿
 Some(p)=q.iter().position(|x|x==k){q.remove(p);q.push_front(k.clone());}}}
 
-fn main(){let 
-c=L::n(3);c.i(1,"one".to_string());c.i(2,"two".to_string());c.i(3,"three".tc=L::n(3);c.i(1,"one".to_string());c.i(2,"two".to_string());ci(3,"three".to_string());thread::scope(|s|{for i in 0..3{let c=&c;s.spawn(move||{for j 
-in 0..5{let k=(i+j)%5;c.i(k,format!("v-{}",k));println!("t{} insert 
-{}",i,k)}for j in 0..5{let k=(i+j)%5;if let Some(v)=c.g(&k){println!("t{} 
-get {}={}",i,k,v)}else{println!("t{} miss 
-{}",i,k)}}});}}).unwrap();println!("\nfinal");for k in 1..6{if let 
+fn﻿main(){let﻿
+c=L::n(3);c.i(1,"one".to_string());c.i(2,"two".to_string());c.i(3,"three".tc=L::n(3);c.i(1,"one".to_string());c.i(2,"two".to_string());ci(3,"three".to_string());thread::scope(|s|{for﻿i﻿in﻿0..3{let﻿c=&c;s.spawn(move||{for﻿j﻿
+in﻿0..5{let﻿k=(i+j)%5;c.i(k,format!("v-{}",k));println!("t{}﻿insert﻿
+{}",i,k)}for﻿j﻿in﻿0..5{let﻿k=(i+j)%5;if﻿let﻿Some(v)=c.g(&k){println!("t{}﻿
+get﻿{}={}",i,k,v)}else{println!("t{}﻿miss﻿
+{}",i,k)}}});}}).unwrap();println!("\nfinal");for﻿k﻿in﻿1..6{if﻿let﻿
 Some(v)=c.g(&k){println!("{}={}",k,v)}else{println!("{}=evicted",k)}}}
